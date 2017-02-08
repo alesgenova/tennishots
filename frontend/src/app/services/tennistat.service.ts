21ab9f5f45efd99fad44129c02b472885e9794dd
Alessandro Genova <ales.genova@gmail.com>
2017-02-08 00:15:27 -0500

    implemented the friends system in the front end! It works pretty good so far

--- frontend/src/app/services/tennistat.service.ts
+++ frontend/src/app/services/tennistat.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
-import { Headers, RequestOptions } from '@angular/http';
+import { Headers, RequestOptions, Response } from '@angular/http';
 import { AuthHttp } from 'angular2-jwt';
 
+import { ProfileForm } from '../objects/registration';
 import { SonyFilter } from '../objects/sonyfilter';
 
 import 'rxjs/add/operator/map';
@@ -48,6 +49,52 @@ export class TennistatService {
       return this.authHttp
           .post(this.ApiUrl+'shotsfilter/',filter, options)
           .map(res => res.json());
+  }
+
+  get_friends(){
+      return this.authHttp
+          .get(this.ApiUrl+'friends/')
+          .map(res => res.json());
+  }
+
+  get_profile(){
+      return this.authHttp
+          .get(this.ApiUrl+'profile/')
+          .map(res => res.json());
+  }
+
+  create_profile(profileForm:ProfileForm){
+      let headers = new Headers({ 'Content-Type': 'application/json' });
+      let options = new RequestOptions({ headers: headers });
+      return this.authHttp.post(this.ApiUrl+'profile/',profileForm, options)
+          .map((response: Response) => {return response.json()} );
+  }
+
+  get_friendrequests(){
+      return this.authHttp
+          .get(this.ApiUrl+'friendrequests/')
+          .map(res => res.json());
+  }
+
+ respond_friendrequest(from_user:string, to_user:string, action:string){
+     let headers = new Headers({ 'Content-Type': 'application/json' });
+     let options = new RequestOptions({ headers: headers });
+     return this.authHttp.post(this.ApiUrl+'friendrequests/',{from_user:from_user, to_user:to_user, action:action}, options)
+         .map((response: Response) => {return response.json()} );
+ }
+
+  add_friend(from_user:string, to_user:string){
+      let headers = new Headers({ 'Content-Type': 'application/json' });
+      let options = new RequestOptions({ headers: headers });
+      return this.authHttp.post(this.ApiUrl+'addfriend/',{from_user:from_user, to_user:to_user}, options)
+          .map((response: Response) => {return response.json()} );
+  }
+
+  search_user(query: string){
+      let headers = new Headers({ 'Content-Type': 'application/json' });
+      let options = new RequestOptions({ headers: headers });
+      return this.authHttp.post(this.ApiUrl+'searchuser/',{query:query}, options)
+          .map((response: Response) => {return response.json()} );
   }
 
 }
