1205990a46727dfc3e2825ed1b820f1419448e2c
Alessandro Genova <ales.genova@gmail.com>
2017-02-11 01:00:15 -0500

    some work towards a provider to store the global loggedIn status and the userProfile

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+//import { loggedIn } from './shared/profile.global';
+import { AuthService } from './services/auth.service';
+import { TennistatService } from './services/tennistat.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+
+  public Profile = Object({});
+/*        username: "",
+        first_name: "",
+        last_name: "",
+        arm: "",
+        units: "",
+        backhand: "",
+        privacy: "",
+        friends: []
+    });*/
+  loggedIn: boolean = false;
+
+  constructor(private authService:AuthService, private tennistatService:TennistatService){}
+
+  ngOnInit(){
+      
+  }
 }
---
+++ frontend/src/app/services/profile.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { TennistatService } from './tennistat.service';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class ProfileService {
+    
+    private loggedIn:boolean;
+    private userProfile:any;
+
+    constructor(private authService:AuthService, private tennistatService:TennistatService) {}
+
+    refreshProfile() {
+        if (this.loggedIn){
+            this.tennistatService.get_profile()
+                  .subscribe(res => this.userProfile = res);
+        }
+    }
+
+    getProfile() {
+        return this.userProfile;
+    }
+}
