bc6010e1e2c885928ae49038f3bb4a9505e151db
Alessandro Genova <ales.genova@gmail.com>
2017-02-14 17:58:21 -0500

    started work on the tag system in the frontend

--- frontend/src/app/labels/labels.component.ts
+++ frontend/src/app/labels/labels.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Label } from '../objects/label';
+import { Period, UserPeriodsList } from '../objects/period';
+
+import { TennistatService } from  '../services/tennistat.service';
+import { ProfileService } from  '../services/profile.service';
 
 @Component({
   selector: 'app-labels',
@@ -7,9 +14,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LabelsComponent implements OnInit {
 
-  constructor() { }
+  userProfile: any;
+  tagList: Label[];
+  sessionList: Period[];
+  doPagination: boolean;
+  nPeriods: number;
+  periodsPerPage: number = 6;
+  currPage: number = 1;
+  periodsSubset: Period[];
+
+  constructor(private tennistatService: TennistatService, private profileService: ProfileService) { }
 
   ngOnInit() {
+      this.userProfile = this.profileService.getProfile();
+      this.refreshTags();
+      this.getSessions();
   }
+
+  refreshTags() {
+      this.tennistatService.get_tags(this.userProfile.user)
+            .subscribe( res => {
+                this.tagList = res;
+            });
+  }
+
+  getSessions() {
+      this.tennistatService.get_periods(this.userProfile.user, "session")
+        .subscribe(data=>{
+            this.sessionList = data;
+            this.nPeriods = this.sessionList.length;
+            this.doPagination = (this.nPeriods > this.periodsPerPage);
+            this.periodsSubset = this.sessionList.slice(0,this.periodsPerPage);
+        });
+  }
+
+  onPageChange(){
+      var start: number;
+      var stop: number;
+      start = (this.currPage-1)*this.periodsPerPage;
+      stop = start+this.periodsPerPage;
+      this.periodsSubset = this.sessionList.slice(start,stop);
+  }
+
+
 
 }
